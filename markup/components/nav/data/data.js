334f90a93d4d222d56a73388de75c288ad06a924
Igor Gorlov <gorlov35@gmail.com>
2022-03-17 09:46:56 +0300

    init

---
+++ markup/components/nav/data/data.js
@@ -0,0 +1,58 @@
+nav: {
+    primaryListRu: [
+        {
+            title: 'Услуги',
+            href: '#services',
+            itemClassMod: ''
+        },
+        {
+            title: 'Кейсы',
+            href: '#cases',
+            itemClassMod: '',
+        },
+        {
+            title: 'О нас',
+            href: '#about',
+            itemClassMod: '',
+        },
+        {
+            title: 'Вакансии',
+            href: '#jobs',
+            itemClassMod: '',
+        },
+        {
+            title: 'Блог',
+            href: '#blog',
+            itemClassMod: ''
+        },
+    ],
+    primaryListEn: [
+        {
+            title: 'Services',
+            href: '#services',
+            itemClassMod: ''
+        },
+        {
+            title: 'Cases',
+            href: '#cases',
+            itemClassMod: '',
+        },
+        {
+            title: 'About Us',
+            href: '#about',
+            itemClassMod: '',
+        },
+        {
+            title: 'Vacancies',
+            href: '#jobs',
+            itemClassMod: '',
+        },
+        {
+            title: 'Blog',
+            href: '#blog',
+            itemClassMod: ''
+        },
+
+    ]
+
+}
